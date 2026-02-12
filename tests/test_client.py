15c246e18cf48093aa5d9a5c6e08222b04e14133
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-02-12 13:39:30 +0300

    tests

---
+++ tests/test_client.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+
+from app.main import app 
+
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get('/')
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
+
+def test_read_docs():
+    response = client.get('/docs')
+    assert response.status_code == 200
+    
+def test_read_docs():
+    response = client.get('/docs')
+    assert response.status_code == 200
