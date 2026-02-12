dbaa312a87d9ede1f8541d64a927fa527d86325e
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-02-12 14:41:03 +0300

    refactor

--- app/main.py
+++ app/main.py
@@ -3,19 +3,20 @@ from datetime import datetime, time, timedelta
 from pydantic import BaseModel, Field
 from typing import Annotated    
 
-class duration(BaseModel):
+class DistanceDuration(BaseModel):
+    distance: Annotated[float, Field(...,lt=100)] = 0
     hours: Annotated[int, Field(...,lt=24)] = 0
-    minutes: Annotated[int, Field(...,lt=60)] = 25
+    minutes: Annotated[int, Field(...,lt=60)] = 50
     seconds: Annotated[int, Field(...,lt=60)] = 0
 
 
 app = FastAPI(title = 'Run toolz')
 
 @app.post('/avg_pace')
-def avgPace(distance: float, duration: duration ):
+def avgPace(data: DistanceDuration ):
     
-    duration_minutes = (60 * duration.hours) + duration.minutes + (duration.seconds / 60)
-    pace = duration_minutes / distance
+    duration_minutes = (60 * data.hours) + data.minutes + (data.seconds / 60)
+    pace = duration_minutes / data.distance
     
     pace_m = str(pace).split('.')[0]
     pace_s = int(str(pace).split('.')[1]) * 60
--- tests/test_avgpace.py
+++ tests/test_avgpace.py
@@ -7,21 +7,23 @@ client = TestClient(app)
 
 def test_read_avg_pace_40():
     data = {
+                "distance": 10,
                 "hours": 0,
                 "minutes": 40,
                 "seconds": 0
             }
-    response = client.post('/avg_pace/?distance=10', json=data)
+    response = client.post('/avg_pace/', json=data)
     assert response.status_code == 200
     assert response.json() == { "pace": "4:0" } 
 
 def test_read_avg_pace_50():
     data = {
+                "distance": 5,
                 "hours": 0,
                 "minutes": 25,
                 "seconds": 0
             }
-    response = client.post('/avg_pace/?distance=5', json=data)
+    response = client.post('/avg_pace/', json=data)
     assert response.status_code == 200
     assert response.json() == { "pace": "5:0" } 
 
